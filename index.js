f22b8b335dfd4ed262b88781edfba060b066673e
Annshuk <annshuks@gmail.com>
2021-10-25 19:55:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ console.log('plus =', plus(34).minus(13).plus(4).value());
 console.log('minus =', minus(5).plus(13).minus(4).value());
 
 console.log('plus =', plus(5).plus(13).minus(4).value());
+
+const addAllInteger =
+  (...args) =>
+  (...args1) =>
+    [...args, ...args1].reduce((next, prev) => next + prev, 0);
+
+console.log(addAllInteger(1, 2, 3, 4, 5, 6)(4, 5, 6));
+
+//ansaawer 1
+const employees = [
+  { name: 'John', salary: '1000' },
+  { name: 'Alex', salary: '1500', isInOffice: true },
+].map((item) => ({ ...item, bonus: 500 }));
+
+const sumEmployeeSalery = employees.reduce((prev, current) => ({
+  salary: +current.salary + +prev.salary,
+}));
+
+const someOneInOffice = employees.filter((item) => item.isInOffice);
+
+console.table(employees);
+console.table(sumEmployeeSalery);
+
+console.warn(someOneInOffice);
